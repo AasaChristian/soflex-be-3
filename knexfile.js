ef1a9a222e259056b4e7566f729bc895ac4512c6
Aasa-Christian <aasachristian@yahoo.com>
2021-05-10 19:14:55 -0400

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,14 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      database: 'soflex2',
+      user:     'postgres',
+      password: 'Mastat20'
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   },
 
@@ -26,19 +31,89 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+      directory: __dirname + '/migrations'
+    },
+
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+
   }
 
+  // production: {
+  //   client: 'pg',
+  //   connection: {
+  //     database: 'd4cjj0k19botpf',
+  //     user:     'ygazklkdpfjwyr',
+  //     password: '2571ff1a9db0dfcfe835a7f2d0f5a6de19afb43f9c4a127f4a5caf227a8e822a'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
 };
+
+
+
+
+// // Update with your config settings.
+
+// module.exports = {
+
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: './dev.sqlite3'
+//     }
+//   },
+
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+// };
