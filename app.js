c618d9b54f430efd6bfcac7f5023fe89ea0e2239
Aasa-Christian <aasachristian@yahoo.com>
2021-05-10 18:20:39 -0400

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
-
+const cors = require('cors')
 const Port = process.env.PORT || 5000;
 
 server.listen(Port, () => {
