30b88dd4230b80c66975543aad248dad73314d43
Aasa-Christian <aasachristian@yahoo.com>
2021-05-10 18:14:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+
+const Port = process.env.PORT || 5000;
+
+server.listen(Port, () => {
+    console.log(`Listening at ${Port}`);
+  });
+
+  var corsOptions = {
+    origin: process.env.BASE_URL || "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    methods: "GET,PUT,POST,DELETE",
+       credentials: false
+}
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ response: "I am alive Test" }).status(200);
+});
